3c811de253614f73dd41c2c99609d7bbe711aba3
swamyishani <swamyishanims@gmail.com>
2026-08-13 22:47:46 +0530

    Mult.asm done

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,48 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+@KBD
+D=M
+@WHITE
+D;JNE
+@BLACK
+D;JEQ
+@LOOP
+0;JMP
+@PIXELS
+M=16384
+(WHITE)
+  @SCREEN
+  D=M
+  (LOOP)
+  @PIXELS
+  M=M+1
+  D=M
+  @SCREEN
+  M=0
+  @DIFF
+  M=24576-D
+  @END
+  M;JEQ
+  @LOOP
+  0;JMP
+@BLACK
+  @SCREEN
+  D=M
+  (LOOP)
+  @PIXELS
+  M=M+1
+  D=M
+  @SCREEN
+  M=-1
+  @DIFF
+  M=24576-D
+  @END
+  M;JEQ
+  @LOOP
+  0;JMP
+(END)
+@END
+0;JMP
+
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,21 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@2
+M=0
+(LOOP)
+  @1
+  D=M
+  @END
+  D;JEQ
+  @1
+  M=M-1
+  @0
+  D=M
+  @2
+  M=M+D
+  @LOOP
+  0;JMP
+(END)
+  @END
+0;JMP
